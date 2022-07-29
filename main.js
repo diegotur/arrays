9eec81a753bf9ac69c46647b71067f7488c10821
diegotur <gpslinea15@hotmail.com>
2022-07-29 13:34:15 -0300

    cabTalar

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 //creo base de datos de choferes
 
-let titulo = document.getElementById("titulo");
-let subtitulo = document.getElementById("subtitulo");
-
-titulo.innerHTML = `<h1>CITACIONES PUENTE URIBURU</h1>`;
-subtitulo.innerHTML = `<h3>CONSULTE LAS CITACIONES SEGUN COCHE</h3>`;
-
-
 class Chofer {
-    constructor(apellido, nombre, legajo, turno, coche){
+    constructor(apellido, nombre, legajo, turno, coche, cabecera){
         this.apellido = apellido;
         this.nombre = nombre;
         this.legajo = legajo;
@@ -18,25 +11,45 @@ class Chofer {
 }
 const choferes = [];    
 
-choferes.push(new Chofer("Alfonso", "Franco", "3100", "Mañana","1"));
-choferes.push(new Chofer("Jimenez", "Leonardo", "2738", "Mañana","2"));
-choferes.push(new Chofer("Soto", "Ariel", "2939", "Mañana","3"));
-choferes.push(new Chofer("Pared", "Fabián", "2936", "Mañana","4"));
-choferes.push(new Chofer("Guibondot", "Hernán", "2749", "Mañana","5"));
-choferes.push(new Chofer("Torrez", "Kevin", "3082", "Tarde","1"));
-choferes.push(new Chofer("Barboza", "Ezequiel", "3005", "Tarde","2"));
-choferes.push(new Chofer("Montesino", "José", "2364", "Tarde","3"));
-choferes.push(new Chofer("Orue", "Salvador", "2498", "Tarde","4"));
-choferes.push(new Chofer("Villalba", "Diego", "2774", "Mañana","7"));
-choferes.push(new Chofer("Lopez", "Nicolás", "3058", "Tarde","7"));
-choferes.push(new Chofer("Quijano", "Alejandro", "2449", "Mañana","8"));
-choferes.push(new Chofer("Santomé", "Julio", "2786", "Tarde","8"));
-choferes.push(new Chofer("Lopez", "Christian", "2801", "Mañana","10"));
-choferes.push(new Chofer("Ramirez", "Juan", "3095", "Tarde","10"));
-choferes.push(new Chofer("Maidana", "Juan", "2437", "Mañana","11"));
-choferes.push(new Chofer("Roqueiro", "Walter", "2689", "Tarde","11"));
-choferes.push(new Chofer("Flores", "Feliciano", "2065", "Mañana","12"));
-choferes.push(new Chofer("Borda", "Eduardo", "2802", "Tarde","12"));
+choferes.push(new Chofer("Alfonso", "Franco Roberto", "3100", "Mañana","1", "Puente Uriburu"));
+choferes.push(new Chofer("Jimenez", "Leonardo Ezequiel", "2738", "Mañana","2", "Puente Uriburu"));
+choferes.push(new Chofer("Soto", "Ariel Abe", "2939", "Mañana","3", "Puente Uriburu"));
+choferes.push(new Chofer("Pared", "Ricardo Fabián", "2936", "Mañana","4", "Puente Uriburu"));
+choferes.push(new Chofer("Guibondot", "Hernán Antonio", "2749", "Mañana","5", "Puente Uriburu"));
+choferes.push(new Chofer("Torrez", "Kevin Luis", "3082", "Tarde","1", "Puente Uriburu"));
+choferes.push(new Chofer("Barboza", "Axel Walter", "3005", "Tarde","2", "Puente Uriburu"));
+choferes.push(new Chofer("Lopez Montecino", "Víctor", "2364", "Tarde","3", "Puente Uriburu"));
+choferes.push(new Chofer("Orue", "Salvador Ángel", "2498", "Tarde","4", "Puente Uriburu"));
+choferes.push(new Chofer("Villalba", "Diego Alejandro", "2774", "Mañana","7", "Puente Uriburu"));
+choferes.push(new Chofer("Lopez", "Nicolás Martín", "3058", "Tarde","7", "Puente Uriburu"));
+choferes.push(new Chofer("Quijano", "Jorge Alejandro", "2449", "Mañana","8", "Puente Uriburu"));
+choferes.push(new Chofer("Santomé", "Néstor Oscar", "2786", "Tarde","8", "Puente Uriburu"));
+choferes.push(new Chofer("Lopez", "Christian Antonio", "2801", "Mañana","10", "Puente Uriburu"));
+choferes.push(new Chofer("Ramirez", "Sergio Eliseo", "3095", "Tarde","10", "Puente Uriburu"));
+choferes.push(new Chofer("Maidana", "Juan Alberto", "2437", "Mañana","11", "Puente Uriburu"));
+choferes.push(new Chofer("Roqueiro", "Walter Darío", "2689", "Tarde","11", "Puente Uriburu"));
+choferes.push(new Chofer("Flores", "Victoriano", "2065", "Mañana","12", "Puente Uriburu"));
+choferes.push(new Chofer("Borda", "Héctor Eduardo", "2802", "Tarde","12", "Puente Uriburu"));
+choferes.push(new Chofer("Ferreyra", "Hugo Ariel", "3006", "Mañana","6", "Talar"));
+choferes.push(new Chofer("Velichco", "Miguel Ángel", "2902", "Tarde","6", "Talar"));
+choferes.push(new Chofer("Ojeda", "Juan Pablo", "2706", "Mañana","9", "Talar"));
+choferes.push(new Chofer("Andino", "Mario Sebastián", "2875", "Tarde","9", "Talar"));
+choferes.push(new Chofer("Mosse", "Federico Matías", "2980", "Mañana","16", "Talar"));
+choferes.push(new Chofer("Dragomir", "Ernesto Fidel", "2624", "Tarde","16", "Talar"));
+choferes.push(new Chofer("Ledesma", "Jesús Feliciano", "2733", "Mañana","32", "Talar"));
+choferes.push(new Chofer("Benitez", "Carlos Matías", "2815", "Tarde","32", "Talar"));
+choferes.push(new Chofer("Gauna", "Gustavo Andrés", "2788", "Mañana","33", "Talar"));
+choferes.push(new Chofer("Sanchez", "Alfredo José", "2512", "Tarde","33", "Talar"));
+choferes.push(new Chofer("Idalgo", "Claudio Norberto", "2898", "Mañana","36", "Talar"));
+choferes.push(new Chofer("García", "Ezequiel", "3016", "Tarde","36", "Talar"));
+choferes.push(new Chofer("Paez", "Cristian Edgardo Nicolás", "3046", "Mañana","38", "Talar"));
+choferes.push(new Chofer("Rodriguez", "Miguel Eduardo", "2526", "Tarde","38", "Talar"));
+choferes.push(new Chofer("Romero", "Enrique Daniel", "2649", "Mañana","39", "Talar"));
+choferes.push(new Chofer("Ruiz", "Adolfo Martín", "2825", "Tarde","39", "Talar"));
+choferes.push(new Chofer("Strefechi", "Ivan Alejandro", "3036", "Mañana","47", "Talar"));
+choferes.push(new Chofer("Albarracín", "Hernán Diego", "2685", "Tarde","47", "Talar"));
+choferes.push(new Chofer("Nuñez", "Luis Omar", "2822", "Mañana","49", "Talar"));
+choferes.push(new Chofer("Avendaño", "Marcos César", "2592", "Tarde","49", "Talar"));
 
 
 //creo base de datos de turnos 
@@ -48,17 +61,33 @@ turnos.push(["2", "00:54", "12:53"]);
 turnos.push(["3", "01:33", "13:00"]);
 turnos.push(["4", "02:07", "13:20"]);
 turnos.push(["5", "02:39", "13:25"]);
-turnos.push(["6", "02:48", "15:26"]);
+turnos.push(["6", "02:48", "13:26"]);
 turnos.push(["7", "02:57", "14:25"]);
 turnos.push(["8", "03:13", "13:57"]);
 turnos.push(["9", "03:27", "13:29"]);
 turnos.push(["11", "03:37", "13:39"]);
 
+const talarTurnos = [];
 
-let modeloCit = [];
+talarTurnos.push(["3", "04:06", "11:25"]);
+talarTurnos.push(["5", "04:22", "11:51"]);
+talarTurnos.push(["8", "04:48", "12:28"]);
+talarTurnos.push(["9", "04:52", "12:49"]);
+talarTurnos.push(["12", "05:15", "13:25"]);
+talarTurnos.push(["13", "05:18", "13:55"]);
+talarTurnos.push(["15", "05:26", "14:50"]);
+talarTurnos.push(["16", "05:30", "13:13"]);
+talarTurnos.push(["17", "05:32", "12:45"]);
+talarTurnos.push(["18", "05:35", "12:57"]);
+
+
 const cantidadDeCoches = ["1","2","3","4","5","7","8","10","11","12"];
-
+let modeloCit = [];
 modeloCit[0] = cantidadDeCoches;
+
+const talarCantidadDeCoches = ["6","9","16","32","33","36","38","39","47","49" ];
+let talarModeloCit = [];
+talarModeloCit[0] = talarCantidadDeCoches;
 
 //Creo un array con modelos de citaciones posibles
 
@@ -69,18 +98,31 @@ for (i=0; i<cantidadDeCoches.length-1; i++){
     x.push(y);
     modeloCit[i + 1] = x;
 }
+for (i=0; i<talarCantidadDeCoches.length-1; i++){
+    let y = talarCantidadDeCoches.at(i);
+    let x =  talarModeloCit[i].map((n) => n);
+    x.shift();
+    x.push(y);
+    talarModeloCit[i + 1] = x;
+}
 
 //elijo al azar el primer coche para el lunes
 
 getIndice = Math.floor ((Math.random() * 10));
+talarGetIndice = Math.floor ((Math.random() * 10));
 
 getPrimero = cantidadDeCoches[getIndice];
 
+talarGetPrimero = talarCantidadDeCoches[talarGetIndice];
+
 const citSemana= [];
+const talarCitSemana= [];
 
 const diaLunes = modeloCit.findIndex((el) => el.at(0) == getPrimero);
+const talarDiaLunes = talarModeloCit.findIndex((el) => el.at(0) == talarGetPrimero);
 
 citSemana[0]= modeloCit[diaLunes];
+talarCitSemana[0]= talarModeloCit[talarDiaLunes];
 
 //creo otro array con las citaciones ordenadas segun primer coche del lunes
 
@@ -92,18 +134,33 @@ for (i=0; i<4; i++){
     citSemana[i + 1] = x;
 }
 
+
+for (i=0; i<4; i++){
+    let y = talarCitSemana[0].at(i);
+    let x =  talarCitSemana[i].map((n) => n);
+    x.shift();
+    x.push(y);
+    talarCitSemana[i + 1] = x;
+}
+
 let getLegajo;
 
 let arrCitacion = [];
+let talarArrCitacion = [];
 
 for (i=0;i<=9;i++){
-    arrCitacion[i] = "columna"+i;   
+    arrCitacion[i] = "columna"+i;
+    talarArrCitacion[i] = "talarColumna"+i;   
 }
 
-arrFilas = [];
+
+let arrFilas = [];
+let talarArrFilas = [];
 
 for (i=0;i<=6;i++){
     arrFilas[i] = "row"+i;
+    talarArrFilas[i] = "talarRow"+i;
+
 }
 
 for (const item of arrCitacion){
@@ -126,6 +183,52 @@ for (const item of arrCitacion){
 
     }
 }
+for (const item of talarArrCitacion){
+    let llenar = document.getElementById(item);
+    let x = talarArrCitacion.indexOf(item);
+    let dato = talarTurnos[x].at(0);
+    
+    let m = document.getElementsByClassName(talarArrFilas.at(0));
+    let m1 = document.getElementsByClassName(talarArrFilas.at(1));
+        
+    m[x].innerHTML = `<td> ${talarTurnos[x].at(1)}</td>`;
+    m1[x].innerHTML = `<td> ${talarTurnos[x].at(2)}</td>`;
+
+    llenar.append(dato);
+
+    for(i=0;i<=4;i++){
+    let y = document.getElementsByClassName(talarArrFilas.at(i+2));
+    
+    y[x].innerHTML = `<td> ${talarCitSemana[i].at(x)}</td>`;
+
+    }
+}
+
+let mostrarPuente = document.getElementById("citacionPuenteLink");
+let mostrarTalar = document.getElementById("citacionTalarLink");
+
+    mostrarPuente.addEventListener("click", mostrar);
+    function mostrar (){
+        document.getElementById("citacionPuente").style.visibility = "visible";
+        document.getElementById("citacionTalar").style.visibility = "hidden";
+    }
+    mostrarTalar.addEventListener("click", mostrar2);
+    function mostrar2 (){
+        document.getElementById("citacionPuente").style.visibility = "hidden";
+        document.getElementById("citacionTalar").style.visibility = "visible";
+        document.getElementById("citacionPuente").style.position = "absolute";
+    }
+
+/* let inputLegajo = document.querySelector("input").value;
+
+let mostrarLegajo = document.getElementById("inputButton");
+
+mostrarLegajo.addEventListener("click", mostrar3);
+function mostrar3 (){
+    document.write (document.querySelector("input").value);
+   
+} */
+
 
 
 //pido legajo por prompt y obtengo sus horarios de toda la semana
